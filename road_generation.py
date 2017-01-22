f7486567f4663b1f3db05b8a1ac8ed56cefafd5e
Hans Kirchner <hans-kirchner@gmx.de>
2017-01-22 16:42:06 +0100

    Add Fitness class

--- road_generation.py
+++ road_generation.py
@@ -3,6 +3,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import heapq
 
 def norm_angle(angle):
     while angle > 2 * math.pi:
@@ -34,11 +35,18 @@ def generate_road(primitives, padding):
 
     return new_primitives
 
-def heuristic(primitives):
-    first_begin = primitives[0].get_beginning()
-    last_end = primitives[-1].get_ending()
-    dist_vec = first_begin[0] - last_end[0]
-    return math.sqrt(dist_vec[0]*dist_vec[0] + dist_vec[1]*dist_vec[1])
+class Fitness:
+    def __init__(self, target_distance):
+        self._target_distance = target_distance
+
+    def fitness(self, primitives):
+        first_begin = primitives[0].get_beginning()
+        last_end = primitives[-1].get_ending()
+        dist_vec = first_begin[0] - last_end[0]
+        dist = math.sqrt(dist_vec[0]*dist_vec[0] + dist_vec[1]*dist_vec[1])
+        dist_weight = abs(dist - self._target_distance) / self._target_distance
+        angle_weight = norm_angle(abs(norm_angle(first_begin[1]) - norm_angle(last_end[1] + math.pi))) / (2*math.pi)
+        return dist_weight + angle_weight
 
 def render_road(primitives):
     plt.axis('equal')
@@ -52,26 +60,24 @@ def render_road(primitives):
 
 if __name__ == "__main__":
     primitives = [
-        primitive.RightCircularArc(10, math.pi /4 ),
-        primitive.RightCircularArc(20, math.pi /4 ),
+        primitive.RightCircularArc(10, math.pi /2 ),
+        primitive.RightCircularArc(10, math.pi /2 ),
+        primitive.RightCircularArc(10, math.pi /2 ),
+        primitive.RightCircularArc(10, math.pi /2 ),
         primitive.StraightLine(5),
-        primitive.LeftCircularArc(10, math.pi / 4 ),
-        primitive.StraightLine(10),
-        primitive.StraightLine(10),
-        primitive.LeftCircularArc(5, math.pi/2 ),
         primitive.StraightLine(5),
-        primitive.LeftCircularArc(5, math.pi)
+        primitive.StraightLine(5),
+        primitive.StraightLine(5)
         #primitive.CircularArc(10, math.pi * 0.5)
         #primitive.StraightLine(20),
     ]
     #render_road(generate_road(primitives, 5))
-    best_h = None
-    best_road = None
-    for i in range(1000):
+    heap = []
+    f = Fitness(1)
+    for i in range(10000):
         random.shuffle(primitives)
-        road = generate_road(primitives, 10)
-        h = heuristic(road)
-        if best_h is None or h < best_h:
-            best_h = h
-            best_road = road
-    render_road(best_road)
+        road = generate_road(primitives, 1)
+        h = f.fitness(road)
+        heapq.heappush(heap, (h, road))
+    for i in range(3):
+        render_road(heap[i][1])
