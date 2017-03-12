b7255da0af6349042fd841d4af9dd85d49728cd3
Hans Kirchner <hans-kirchner@gmx.de>
2017-03-12 23:00:34 +0100

    Render obstacles and stop lines

--- commonroad/renderer/groundplane.py
+++ commonroad/renderer/groundplane.py
@@ -2,7 +2,7 @@ import cairo
 import math
 from commonroad import utils
 
-PIXEL_PER_UNIT = 5000
+PIXEL_PER_UNIT = 1000
 
 def draw_boundary(ctx, boundary):
     if boundary.lineMarking is None:
@@ -10,12 +10,55 @@ def draw_boundary(ctx, boundary):
     ctx.set_line_width (0.02)
     if boundary.lineMarking == "dashed":
         ctx.set_dash([0.2, 0.2])
+    else:
+        ctx.set_dash([])
 
     ctx.move_to(boundary.point[0].x, boundary.point[0].y)
-    for p in boundary.point:
+    for p in boundary.point[1:]:
         ctx.line_to(p.x, p.y)
-
     ctx.stroke()
+
+def draw_stop_line(ctx, lanelet):
+    p1 = lanelet.leftBoundary.point[-1]
+    p2 = lanelet.rightBoundary.point[-1]
+
+    if lanelet.stopLine:
+        if lanelet.stopLine == "dashed":
+            ctx.set_dash([0.1, 0.1])
+        else:
+            ctx.set_dash([])
+        ctx.move_to(p1.x, p1.y)
+        ctx.line_to(p2.x, p2.y)
+        ctx.stroke()
+
+def draw_rectangle(ctx, rectangle):
+    ctx.save()
+    ctx.translate(rectangle.centerPoint.x, rectangle.centerPoint.y)
+    ctx.rotate(-rectangle.orientation)
+    ctx.rectangle(- rectangle.width / 2, - rectangle.length / 2, rectangle.width, rectangle.length)
+    ctx.fill()
+    ctx.restore()
+
+def draw_circle(ctx, circle):
+    ctx.arc(circle.centerPoint.x, circle.centerPoint.y, circle.radius, 0, 2*math.pi)
+    ctx.fill()
+
+def draw_polygon(ctx, polygon):
+    ctx.move_to(polygon.point[0].x, polygon.point[1].y)
+    for point in polygon.point[1:]:
+        ctx.line_to(point.x, point.y)
+    ctx.fill()
+
+def draw_shape(ctx, shape):
+    for rect in shape.rectangle:
+        draw_rectangle(ctx, rect)
+    for circ in shape.circle:
+        draw_circle(ctx, circ)
+    for poly in shape.polygon:
+        draw_polygon(ctx, poly)
+
+def draw_obstacle(ctx, obstacle):
+    draw_shape(ctx, obstacle.shape)
 
 def draw(doc):
     bounding_box = utils.get_bounding_box(doc)
@@ -40,5 +83,9 @@ def draw(doc):
     for lanelet in doc.lanelet:
         draw_boundary(ctx, lanelet.leftBoundary)
         draw_boundary(ctx, lanelet.rightBoundary)
+        draw_stop_line(ctx, lanelet)
+
+    for obstacle in doc.obstacle:
+        draw_obstacle(ctx, obstacle)
 
     return surface
--- commonroad/utils.py
+++ commonroad/utils.py
@@ -28,6 +28,29 @@ def get_bounding_box(object):
         left = get_bounding_box(object.leftBoundary)
         right = get_bounding_box(object.rightBoundary)
         return left.union(right)
+    elif isinstance(object, schema.rectangle):
+        radius = math.sqrt((object.width/2)**2 + (object.length/2)**2)
+        return BoundingBox(
+            object.centerPoint.x - radius,
+            object.centerPoint.y - radius,
+            object.centerPoint.x + radius,
+            object.centerPoint.y + radius)
+    elif isinstance(object, schema.circle):
+        return BoundingBox(
+            object.centerPoint.x - radius,
+            object.centerPoint.y - radius,
+            object.centerPoint.x + radius,
+            object.centerPoint.y + radius)
+    elif isinstance(object, schema.polygon):
+        all_x = list(map(lambda p: p.x, object.point))
+        all_y = list(map(lambda p: p.y, object.point))
+        return BoundingBox(min(all_x), min(all_y), max(all_x), max(all_y))
+    elif isinstance(object, schema.shape):
+        # TODO
+        raise NotImplementedError()
+    elif isinstance(object, schema.obstacle):
+        # TODO
+        raise NotImplementedError()
     elif isinstance(object, schema.CommonRoad):
         return reduce(
             lambda x,y: x.union(y),
