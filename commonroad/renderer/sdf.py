e95b8feedca001e61da1f8cca2326aa55e1026e7
Hans Kirchner <hans-kirchner@gmx.de>
2017-03-17 16:15:14 +0100

    Refactor gazebo renderer. Use argparse.

--- commonroad/renderer/sdf.py
+++ commonroad/renderer/sdf.py
@@ -2,10 +2,8 @@ from commonroad.renderer import groundplane, obstacle
 from commonroad import schema
 from os import path
 
-def generate_sdf(xml_file, target_dir):
-    with open(xml_file, "r") as file:
-        content = file.read()
-    doc = schema.CreateFromDocument(content)
+def generate_sdf(xml_content, target_dir):
+    doc = schema.CreateFromDocument(xml_content)
 
     content = groundplane.draw(doc, target_dir)
     for obst in doc.obstacle:
--- groundplane_test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from commonroad.renderer import sdf
-import sys
-
-if __name__ == "__main__":
-    sdf.generate_sdf(sys.argv[1], sys.argv[2])
