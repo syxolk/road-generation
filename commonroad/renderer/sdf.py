05d8d24bc160e6bf1f0702e01dba985730e8db6f
Hans Kirchner <hans-kirchner@gmx.de>
2017-03-20 14:02:57 +0100

    Add sun light

--- commonroad/renderer/sdf.py
+++ commonroad/renderer/sdf.py
@@ -15,5 +15,23 @@ def generate_sdf(xml_content, target_dir):
 
     with open(path.join(target_dir, "world.sdf"), "w") as file:
         file.write("<sdf version='1.6'><world name='default'>")
+        file.write(sun_light())
         file.write(content)
         file.write("</world></sdf>")
+
+def sun_light():
+    return """
+    <light name='sun_light' type='directional'>
+      <pose frame=''>0 0 1 0 -0 0</pose>
+      <diffuse>0.5 0.5 0.5 1</diffuse>
+      <specular>0.1 0.1 0.1 1</specular>
+      <direction>0.1 0.1 -0.9</direction>
+      <attenuation>
+        <range>20</range>
+        <constant>0.5</constant>
+        <linear>0.01</linear>
+        <quadratic>0.001</quadratic>
+      </attenuation>
+      <cast_shadows>1</cast_shadows>
+    </light>
+    """
