5b737a925135f0dad7e43e8a3c88c1d84f61e1b4
Hans Kirchner <hans-kirchner@gmx.de>
2017-04-03 04:45:26 +0200

    Add camera sensor

--- commonroad/renderer/ego_vehicle.py
+++ commonroad/renderer/ego_vehicle.py
@@ -17,6 +17,23 @@ def draw(target_dir):
             <mesh><uri>file://{0}</uri></mesh>
           </geometry>
         </visual>
+        <sensor type="camera" name="my_sensor">
+          <pose>0 0 0.32 0 0.7 0</pose>
+          <camera>
+            <horizontal_fov>1.047</horizontal_fov>
+            <image>
+              <width>320</width>
+              <height>240</height>
+            </image>
+            <clip>
+              <near>0.1</near>
+              <far>100</far>
+            </clip>
+          </camera>
+          <always_on>1</always_on>
+          <update_rate>30</update_rate>
+          <visualize>true</visualize>
+        </sensor>
       </link>
     </model>
     """.format(model_file)
